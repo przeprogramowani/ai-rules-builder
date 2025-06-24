1e5ddc3850def432e37d0312795641411c21ba6c
Paweł Gnat <104066590+Pawel-Gnat@users.noreply.github.com>
2025-06-24 13:23:44 +0200

    feat: add tooltip component (#61)
    * feat: add tooltip component
    
    * chore: revert svg path change

--- src/components/rule-preview/RulesPreviewActions.tsx
+++ src/components/rule-preview/RulesPreviewActions.tsx
@@ -2,19 +2,25 @@ import { ExternalLink } from 'lucide-react';
 import React from 'react';
 import { aiEnvironmentConfig } from '../../data/ai-environments.ts';
 import { useProjectStore } from '../../store/projectStore';
+import Tooltip from '../ui/Tooltip.tsx';
 
 export const RulesPreviewActions: React.FC<unknown> = () => {
   const { selectedEnvironment } = useProjectStore();
 
   return (
-    <a
-      href={aiEnvironmentConfig[selectedEnvironment].docsUrl}
-      target="_blank"
-      className="px-3 py-1 bg-purple-700 text-white rounded-md hover:bg-purple-600 flex items-center text-sm opacity-40 hover:opacity-100 cursor-pointer"
-      aria-label={`Open documentation for ${selectedEnvironment}`}
+    <Tooltip
+      content={`Open documentation for ${selectedEnvironment.charAt(0).toUpperCase() + selectedEnvironment.slice(1)}`}
+      position="bottom"
     >
-      <ExternalLink className="h-4 w-4" />
-    </a>
+      <a
+        href={aiEnvironmentConfig[selectedEnvironment].docsUrl}
+        target="_blank"
+        className="px-3 py-1 bg-purple-700 text-white rounded-md hover:bg-purple-600 flex items-center text-sm opacity-40 hover:opacity-100 cursor-pointer"
+        aria-label={`Open documentation for ${selectedEnvironment}`}
+      >
+        <ExternalLink className="h-4 w-4" />
+      </a>
+    </Tooltip>
   );
 };
 
