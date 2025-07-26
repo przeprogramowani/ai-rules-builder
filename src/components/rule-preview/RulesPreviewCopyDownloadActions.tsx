1eb1d27f55ca1e422c7ecfd7663ce191454a4b30
psmyrdek <przemek.smyrdek@gmail.com>
2025-07-26 10:10:18 +0200

    chore: icons instead of svg

--- src/components/rule-preview/RulesPreviewCopyDownloadActions.tsx
+++ src/components/rule-preview/RulesPreviewCopyDownloadActions.tsx
@@ -1,4 +1,5 @@
 import { type Zippable, zipSync } from 'fflate';
+import { Check, Copy, Download } from 'lucide-react';
 import React, { Fragment, useState } from 'react';
 import { aiEnvironmentConfig } from '../../data/ai-environments.ts';
 import type { RulesContent } from '../../services/rules-builder/RulesBuilderTypes.ts';
@@ -110,25 +111,7 @@ export const RulesPreviewCopyDownloadActions: React.FC<RulesPreviewCopyDownloadA
           } rounded-md flex items-center transition-colors duration-200 text-sm opacity-40 hover:opacity-100 cursor-pointer`}
           aria-label="Copy"
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-4 w-4"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-          >
-            {showCopiedMessage ? (
-              <path
-                fillRule="evenodd"
-                d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                clipRule="evenodd"
-              />
-            ) : (
-              <path d="M8 3a1 1 0 011-1h2a1 1 0 110 2H9a1 1 0 01-1-1z" />
-            )}
-            {!showCopiedMessage && (
-              <path d="M6 3a2 2 0 00-2 2v11a2 2 0 002 2h8a2 2 0 002-2V5a2 2 0 00-2-2 3 3 0 01-3 3H9a3 3 0 01-3-3z" />
-            )}
-          </svg>
+          {showCopiedMessage ? <Check className="h-4 w-4" /> : <Copy className="h-4 w-4" />}
         </button>
       </Tooltip>
       <Tooltip
@@ -140,18 +123,7 @@ export const RulesPreviewCopyDownloadActions: React.FC<RulesPreviewCopyDownloadA
           className="px-3 py-1 bg-indigo-700 text-white rounded-md hover:bg-indigo-600 flex items-center text-sm opacity-40 hover:opacity-100 cursor-pointer"
           aria-label="Download"
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-4 w-4"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-          >
-            <path
-              fillRule="evenodd"
-              d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-              clipRule="evenodd"
-            />
-          </svg>
+          <Download className="h-4 w-4" />
         </button>
       </Tooltip>
     </Fragment>
